0541b4a6160a019f101f334565b601eb82339bb8
Favio <121990387+DjNoc77@users.noreply.github.com>
2023-12-12 15:02:34 -0600

    FIXED HERE

--- src/main/java/com/Revpay/FavePay/FavepayApplication.java
+++ src/main/java/com/Revpay/FavePay/FavepayApplication.java
@@ -8,6 +8,7 @@ public class FavepayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavepayApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
